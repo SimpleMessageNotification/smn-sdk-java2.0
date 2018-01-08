2b608063e7269c45f65f51bf0af90e5f07521a16
breakawayz <375831500@qq.com>
2018-01-08 19:51:36 +0800

    添加publish相关操作

---
+++ smn-sdk-java/src/main/java/com/smn/request/publish/PublishWithStructureRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018. Huawei Technologies Co., LTD. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License, Version 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License, Version 2.0 for more details.
+ */
+package com.smn.request.publish;
+
+import com.smn.common.Constants;
+import com.smn.http.HttpMethod;
+import com.smn.request.AbstractRequest;
+import com.smn.response.publish.PublishResponse;
+import com.smn.util.JsonUtil;
+import com.smn.util.StringUtil;
+import com.smn.util.ValidationUtil;
+
+import java.util.Map;
+
+/**
+ * the request of publish with structure
+ *
+ * @author zhangyx
+ * @version 2.0.0
+ */
+public class PublishWithStructureRequest extends AbstractRequest<PublishResponse> {
+    /**
+     * topic urn
+     */
+    private String topicUrn;
+
+    /**
+     * subject
+     */
+    private String subject;
+
+    /**
+     * message_structure
+     */
+    private Map<String, Object> messageStructure;
+
+    @Override
+    public HttpMethod getHttpMethod() {
+        return HttpMethod.POST;
+    }
+
+    @Override
+    public String getUrl() {
+        if (StringUtil.isEmpty(topicUrn)) {
+            throw new NullPointerException("topic urn is null");
+        }
+
+        if (!ValidationUtil.validateSubject(subject)) {
+            throw new IllegalArgumentException("subject is invalid");
+        }
+
+        if (!ValidationUtil.validateMessageStructureLength((String)this.bodyMap.get("message_structure"))) {
+            throw new IllegalArgumentException("message structure is oversize");
+        }
+
+        if (!ValidationUtil.validateMessageStructureDefault(messageStructure)) {
+            throw new IllegalArgumentException("message structure not contain default message");
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(getSmnServiceUrl());
+        sb.append(Constants.URL_DELIMITER).append(Constants.V2).append(Constants.URL_DELIMITER)
+                .append(projectId).append(Constants.URL_DELIMITER).append(Constants.SMN_NOTIFICATIONS)
+                .append(Constants.URL_DELIMITER).append(Constants.TOPICS)
+                .append(Constants.URL_DELIMITER).append(topicUrn)
+                .append(Constants.URL_DELIMITER).append(Constants.PUBLISH);
+        return sb.toString();
+    }
+
+    public PublishWithStructureRequest setTopicUrn(String topicUrn) {
+        this.topicUrn = topicUrn;
+        return this;
+    }
+
+    public PublishWithStructureRequest setSubject(String subject) {
+        this.subject = subject;
+        this.bodyMap.put("subject", subject);
+        return this;
+    }
+
+    public PublishWithStructureRequest setMessageStructure(Map<String, Object> messageStructure) {
+        this.messageStructure = messageStructure;
+        this.bodyMap.put("message_structure", JsonUtil.getJsonStringByMap(messageStructure));
+        return this;
+    }
+
+    public String getTopicUrn() {
+        return topicUrn;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Map<String, Object> getMessageStructure() {
+        return messageStructure;
+    }
+}
---
+++ smn-sdk-java/src/main/java/com/smn/response/publish/PublishResponse.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2018. Huawei Technologies Co., LTD. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License, Version 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License, Version 2.0 for more details.
+ */
+package com.smn.response.publish;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.smn.response.AbstractResponse;
+
+/**
+ * the response data of publish
+ *
+ * @author zhangyx
+ * @version 2.0.0
+ */
+public class PublishResponse extends AbstractResponse {
+
+    @JsonProperty("message_id")
+    private String messageId;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+}
